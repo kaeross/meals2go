37f3d592585db31b05139a91e0bc92261e370b22
Kate Ross <kate.ross@simpplr.com>
2022-07-03 20:28:42 +0100

    add map region zoom

--- src/features/map/screens/MapScreen.tsx
+++ src/features/map/screens/MapScreen.tsx
@@ -1,21 +1,60 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { LocationsContext } from "../../../services/locations/locationsContext";
 import { RestaurantsContext } from "../../../services/restaurants/restaurantsContext";
+import { GeoLocation, Geometry } from "../../../services/types";
 import { Search } from "../../../shared/components/Search";
 
 export const MapScreen = () => {
+  const [latitudeDelta, setLatitudeDelta] = useState(0);
+  const [longitudeDelta, setLongitudeDelta] = useState(0);
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
   const { restaurants } = useContext(RestaurantsContext);
 
-  const { isLoading, location, search } = useContext(LocationsContext);
+  const { location, viewport, search } = useContext(LocationsContext);
+
+  const setDeltas = (vp: Geometry["viewport"]) => {
+    const {
+      northeast: { lat: northeastLat, lng: northeastLng },
+      southwest: { lat: southwestLat, lng: southwestLng },
+    } = vp;
+
+    setLatitudeDelta(northeastLat - southwestLat);
+    setLongitudeDelta(northeastLng - southwestLng);
+  };
+
+  const setLatLng = (loc: GeoLocation) => {
+    const { lat, lng } = loc;
+    setLatitude(lat);
+    setLongitude(lng);
+  };
+
+  useEffect(() => {
+    if (!viewport || !location) {
+      return;
+    }
+    setDeltas(viewport);
+    setLatLng(location);
+  }, [location, viewport]);
 
   return (
     <>
       <View style={styles.search}>
         <Search onChangeSearch={search} />
       </View>
-      <MapView style={styles.map} showsUserLocation={true}>
+      <MapView
+        style={styles.map}
+        showsUserLocation={true}
+        region={{
+          latitudeDelta,
+          longitudeDelta: 0.2,
+          latitude,
+          longitude,
+        }}
+      >
         {restaurants &&
           restaurants.map(
             (
